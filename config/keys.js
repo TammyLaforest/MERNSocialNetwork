5e90c391b5228d6951590678877fd92851da2136
TammyLaforest <TammyLaforest@gmail.com>
2019-03-05 13:13:51 -0500

    validation of registration and login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   // Temporary database for development mode
-  mongoURI: "mongodb://user:Password123!@ds213183.mlab.com:13183/devconnectortl"
+  mongoURI:
+    "mongodb://user:Password123!@ds213183.mlab.com:13183/devconnectortl",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-// main app route
-app.get("/", (req, res) => res.send("Hello"));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// process is for Heroku. 5000 is for localhost
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Password middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport.js")(passport);
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
