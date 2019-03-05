359ba7b966c8c8a1efac7e7d4f50e3f53a481ede
TammyLaforest <TammyLaforest@gmail.com>
2019-03-05 13:49:57 -0500

    corrected syntax errors

--- models/Profile.js
+++ models/Profile.js
@@ -27,109 +27,94 @@ const ProfileSchema = new Schema({
     required: true
   },
   skills: {
-      type: [String],
-      required: true
+    type: [String],
+    required: true
   },
   bio: {
-      type: String
-  }
+    type: String
+  },
   githubusername: {
-      type: String
+    type: String
   },
   experience: [
-      {
-        title: 
-        {
-            type: String,
-            required: true
-        },
-        company: 
-        {
-            type: String,
-            required: true
-        },
-        location: 
-        {
-            type: String
-        },
-        from: 
-        {
-            type: Date,
-            required: true
-        },
-        to: 
-        {
-            type: Date
-        },
-        current: 
-        {
-            type: Boolean,
-            default: false
-        },
-        description: 
-        {
-            type: String
-        }
-    }
-],
-education: [
     {
-      school: 
-      {
-          type: String,
-          required: true
+      title: {
+        type: String,
+        required: true
       },
-      degree: 
-      {
-          type: String,
-          required: true
+      company: {
+        type: String,
+        required: true
       },
-      fieldofstudy: 
-      {
-          type: String,
-          required: true
-      },
-      from: 
-      {
-          type: Date,
-          required: true
-      },
-      to: 
-      {
-          type: Date
-      },
-      current: 
-      {
-          type: Boolean,
-          default: false
-      },
-      description: 
-      {
-          type: String
-      }
-  }
-],
-social: {
-    youtube: {
+      location: {
         type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
     },
     twitter: {
-        type: String
+      type: String
     },
     facebook: {
-        type: String
+      type: String
     },
     linkedin: {
-        type: String
+      type: String
     },
     instagram: {
-        type: String
+      type: String
     }
-},
-date: {
+  },
+  date: {
     type: date,
     default: Date.now
-}
-
+  }
 });
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
